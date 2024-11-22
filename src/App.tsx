353d2ec82bb81ca3111c900a0ac1bb958f2d06f5
shwetank-1708 <shwetank.chauhan17@gmail.com>
2024-11-22 13:32:20 +0530

    Added React-Icons

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./redux/store";
 import { addTask, removeTask } from "./redux/slices/tasksSlice";
 import "./App.css";
+import { RiCloseCircleLine } from "react-icons/ri";
+import { TbPointFilled } from "react-icons/tb";
 
 function App() {
   const [task, setTask] = useState("");
@@ -22,21 +24,43 @@ function App() {
 
   return (
     <div>
-      <h1>To Do App</h1>
-      <input
-        type="text"
-        placeholder="Enter Your Task"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-      />
-      <button onClick={addTaskHandler}>Add</button>
-      <ul>
-        {tasks.map((t, index) => (
-          <li key={index}>
-            {t} <button onClick={() => removeTaskHandler(t)}>Remove</button>
-          </li>
-        ))}
-      </ul>
+      <div className="flex flex-col justify-center items-center h-[100vh]">
+        <h1 className="text-4xl font-bold m-4">To Do App</h1>
+        <div className="flex gap-2">
+          <input
+            className="bg-gray-100 p-2 rounded-full"
+            type="text"
+            placeholder="Enter Your Task"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+          <button
+            onClick={addTaskHandler}
+            className="bg-sky-200 py-2 px-5 rounded-full"
+          >
+            Add
+          </button>
+        </div>
+        <ul>
+          {tasks.map((t, index) => (
+            <li
+              key={index}
+              className="flex items-center justify-around w-[500px] m-2 bg-gray-400"
+            >
+              <div className="flex items-center">
+                <TbPointFilled />
+                {t}{" "}
+              </div>
+              <div>
+                <RiCloseCircleLine
+                  onClick={() => removeTaskHandler(t)}
+                  className="cursor-pointer"
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
